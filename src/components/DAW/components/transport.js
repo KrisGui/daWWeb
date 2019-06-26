72ec28bc0179012244592e6e4a0b311f1a337cf0
Dmitri Larionov <larionovdmitri@gmail.com>
2019-06-26 00:26:06 -0500

    extracted tone js into react context

--- src/components/DAW/components/transport.js
+++ src/components/DAW/components/transport.js
@@ -1,250 +1,280 @@
-import React, {Component} from 'react'
-import Tone from 'tone'
+import React, { Component } from "react";
+import WebDawButton from "./WebDawButton";
+import Tone from "tone";
 
 class Transport extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    //decorate component with tonejs functionality
+    //todo extract into decorator pattern
+    this.toneJsContext = props.toneJsContext;
 
     this.state = {
       bpm: Tone.Transport.bpm.value.toFixed(2),
-      bpmInput: '',
-      metronomeAnimation: '○●',
-      beats: +Tone.Transport.position.split(':')[0] + 1,
-      bars: +Tone.Transport.position.split(':')[1] + 1,
-      sixteenths: +Tone.Transport.position.split(':')[2][0] + 1,
-    }
+      bpmInput: "",
+      metronomeAnimation: "○●",
+      beats: +Tone.Transport.position.split(":")[0] + 1,
+      bars: +Tone.Transport.position.split(":")[1] + 1,
+      sixteenths: +Tone.Transport.position.split(":")[2][0] + 1
+    };
 
-    this.metronome = new Tone.Synth({
-      oscillator: {type: 'sawtooth'},
-      envelope: {
-        decay: 0.005,
-        sustain: 0.005,
-        release: 0.005
-      }
-    })
+    this.metronome = this.toneJsContext.createSynth();
 
-    this.taps = []
-    this.tapWatch = new Tone.TickSource(1).start(0)
+    this.taps = [];
+    this.tapWatch = new Tone.TickSource(1).start(0);
 
     Tone.Transport.scheduleRepeat(time => {
-      const note = Tone.Transport.position.split(':')[1] === '0' ? 'A#5' : 'C5'
-      this.metronome.triggerAttackRelease(note, '32n', time)
-    }, '4n')
+      const note = Tone.Transport.position.split(":")[1] === "0" ? "A#5" : "C5";
+      this.metronome.triggerAttackRelease(note, "32n", time);
+    }, "4n");
 
-    this.metronomeFrames = ['●○', '○●']
-    this.metronomeFrame = 0
+    this.metronomeFrames = ["●○", "○●"];
+    this.metronomeFrame = 0;
     Tone.Transport.scheduleRepeat(time => {
       Tone.Draw.schedule(() => {
         this.setState({
-          metronomeAnimation: this.metronomeFrames[this.metronomeFrame % this.metronomeFrames.length]
-        })
-        this.metronomeFrame++
-      }, time)
-    }, '4n')
+          metronomeAnimation: this.metronomeFrames[
+            this.metronomeFrame % this.metronomeFrames.length
+          ]
+        });
+        this.metronomeFrame++;
+      }, time);
+    }, "4n");
 
     Tone.Transport.scheduleRepeat(() => {
       Tone.Draw.schedule(() => {
         this.setState({
-          beats: +Tone.Transport.position.split(':')[0] + 1,
-          bars: +Tone.Transport.position.split(':')[1] + 1,
-          sixteenths: +Tone.Transport.position.split(':')[2][0] + 1,
-        })
-      })
-    }, '32n')
+          beats: +Tone.Transport.position.split(":")[0] + 1,
+          bars: +Tone.Transport.position.split(":")[1] + 1,
+          sixteenths: +Tone.Transport.position.split(":")[2][0] + 1
+        });
+      });
+    }, "32n");
   }
 
   componentDidMount() {
-    document.querySelectorAll('button').forEach(button => {
-      button.addEventListener('mousedown', e => e.preventDefault())
-    })
+    document.querySelectorAll("button").forEach(button => {
+      button.addEventListener("mousedown", e => e.preventDefault());
+    });
 
     // Spacebar to toggle transport, will it interfere with track naming?
-    document.addEventListener('keydown', e => {
+    document.addEventListener("keydown", e => {
       if (e.keyCode === 32) {
-        if (Tone.Transport.state === 'started') this.stopTransport()
-        else this.toggleTransport()
+        if (Tone.Transport.state === "started") this.stopTransport();
+        else this.toggleTransport();
       }
-    })
+    });
   }
 
   componentWillUnmount() {
-    this.tapWatch.dispose()
-    this.metronome.dispose()
+    this.tapWatch.dispose();
+    this.metronome.dispose();
 
-    document.querySelectorAll('button').forEach(button => {
-      button.removeEventListener('mousedown', e => e.preventDefault())
-    })
+    document.querySelectorAll("button").forEach(button => {
+      button.removeEventListener("mousedown", e => e.preventDefault());
+    });
 
-    document.removeEventListener('keydown', e => {
+    document.removeEventListener("keydown", e => {
       if (e.keyCode === 32) {
-        if (Tone.Transport.state === 'started') this.stopTransport()
-        else this.toggleTransport()
+        if (Tone.Transport.state === "started") this.stopTransport();
+        else this.toggleTransport();
       }
-    })
+    });
   }
 
   setBPM = newBPM => {
     isNaN(newBPM)
-      ? newBPM = this.state.bpm
+      ? (newBPM = this.state.bpm)
       : newBPM < 30
-        ? Tone.Transport.bpm.value = 30
-        : newBPM > 300
-          ? Tone.Transport.bpm.value = 300
-          : Tone.Transport.bpm.value = newBPM
-    this.setState({bpm: (Math.round(Tone.Transport.bpm.value * 100) / 100).toFixed(2)})
-  }
+      ? (Tone.Transport.bpm.value = 30)
+      : newBPM > 300
+      ? (Tone.Transport.bpm.value = 300)
+      : (Tone.Transport.bpm.value = newBPM);
+    this.setState({
+      bpm: (Math.round(Tone.Transport.bpm.value * 100) / 100).toFixed(2)
+    });
+  };
 
   inputBPM = e => {
-    this.setState({[e.target.id]: e.target.value})
-  }
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
   blurInput = e => {
-    this.setState({[e.target.id]: ''})
-  }
+    this.setState({ [e.target.id]: "" });
+  };
 
   submitBPM = e => {
     if (e.keyCode === 13) {
-      this.setBPM(e.target.value)
-      this.setState({bpmInput: ''})
-      e.target.blur()
+      this.setBPM(e.target.value);
+      this.setState({ bpmInput: "" });
+      e.target.blur();
     }
-  }
+  };
 
   adjustBPM = e => {
-    const value = e.shiftKey ? .01 : 1
-    const direction = e.target.id === 'increment' ? value : -value
-    this.setBPM(Tone.Transport.bpm.value + direction)
+    const value = e.shiftKey ? 0.01 : 1;
+    const direction = e.target.id === "increment" ? value : -value;
+    this.setBPM(Tone.Transport.bpm.value + direction);
     this.adjustTimeout = setTimeout(() => {
       this.adjustInterval = setInterval(() => {
-        this.setBPM(Tone.Transport.bpm.value + direction)
-      }, 30)
-    }, 500)
-  }
+        this.setBPM(Tone.Transport.bpm.value + direction);
+      }, 30);
+    }, 500);
+  };
 
   clearAdjustTimers = () => {
-    clearTimeout(this.adjustTimeout)
-    clearInterval(this.adjustInterval)
-  }
+    clearTimeout(this.adjustTimeout);
+    clearInterval(this.adjustInterval);
+  };
 
   tapBPM = e => {
-    clearTimeout(this.tapTimeout)
+    clearTimeout(this.tapTimeout);
     this.tapTimeout = setTimeout(() => {
-      this.taps = []
-    }, 2000)
-    
-    let tappedBPM = 0
-    this.taps.push(this.tapWatch.seconds)
-    this.tapWatch.stop().start()
+      this.taps = [];
+    }, 2000);
+
+    let tappedBPM = 0;
+    this.taps.push(this.tapWatch.seconds);
+    this.tapWatch.stop().start();
     if (this.taps.length > 1) {
-      const tapsTransformed = this.taps.slice(1)
-      tappedBPM = 60 / ((tapsTransformed.reduce((a, c) => a + c, 0)) / tapsTransformed.length)
-      this.setBPM(tappedBPM)
+      const tapsTransformed = this.taps.slice(1);
+      tappedBPM =
+        60 /
+        (tapsTransformed.reduce((a, c) => a + c, 0) / tapsTransformed.length);
+      this.setBPM(tappedBPM);
     }
-    e.target.className = 'active'
-  }
+    e.target.className = "active";
+  };
 
   clearActiveClass = e => {
-    e.target.className = ''
-  }
+    e.target.className = "";
+  };
 
   toggleMetronome = e => {
-    e.target.classList.toggle('active')
-    e.target.classList.contains('active')
+    e.target.classList.toggle("active");
+    e.target.classList.contains("active")
       ? this.metronome.toMaster()
-      : this.metronome.disconnect(Tone.Master)
-  }
+      : this.metronome.disconnect(Tone.Master);
+  };
 
   toggleTransport = () => {
-    document.getElementById('stop').className = ''
-    const playPause = document.getElementById('play-pause')
-    switch(Tone.Transport.state) {
-      case 'started':
-        Tone.Transport.pause()
-        playPause.className = 'paused'
-        break
+    document.getElementById("stop").className = "";
+    const playPause = document.getElementById("play-pause");
+    switch (Tone.Transport.state) {
+      case "started":
+        Tone.Transport.pause();
+        playPause.className = "paused";
+        break;
       default:
-        Tone.Transport.start()
-        playPause.className = 'active'
+        Tone.Transport.start();
+        playPause.className = "active";
     }
-  }
+  };
 
   stopTransport = () => {
-    document.getElementById('play-pause').className = ''
-    const stop = document.getElementById('stop')
-    Tone.Transport.stop()
-    this.setState({transportPosition: Tone.Transport.position})
-    stop.className = 'active'
-  }
+    document.getElementById("play-pause").className = "";
+    const stop = document.getElementById("stop");
+    Tone.Transport.stop();
+    this.setState({ transportPosition: Tone.Transport.position });
+    stop.className = "active";
+  };
 
   render() {
     return (
-      <div className='Transport container'>
+      <div className="Transport container">
+        <div className="tempo controller">
+          <WebDawButton 
+          git/>
+          <button
+            type="button"
+            id="tap"
+            onMouseDown={this.tapBPM}
+            onMouseUp={this.clearActiveClass}
+            onMouseLeave={this.clearActiveClass}
+          >
+            {" "}
+            Tap{" "}
+          </button>
 
-        <div className='tempo controller'>
-          <button type='button'
-                  id='tap'
-                  onMouseDown={this.tapBPM}
-                  onMouseUp={this.clearActiveClass}
-                  onMouseLeave={this.clearActiveClass}> Tap </button>
+          <button
+            type="button"
+            id="decrement"
+            onMouseDown={this.adjustBPM}
+            onMouseUp={this.clearAdjustTimers}
+            onMouseLeave={this.clearAdjustTimers}
+          >
+            {" "}
+            -{" "}
+          </button>
 
-          <button type='button'
-                  id='decrement'
-                  onMouseDown={this.adjustBPM}
-                  onMouseUp={this.clearAdjustTimers}
-                  onMouseLeave={this.clearAdjustTimers}> - </button>
+          <input
+            type="number"
+            id="bpmInput"
+            placeholder={this.state.bpm}
+            value={this.state.bpmInput}
+            onChange={this.inputBPM}
+            onKeyUp={this.submitBPM}
+            onBlur={this.blurInput}
+          />
 
-          <input  type='number'
-                  id='bpmInput'
-                  placeholder={this.state.bpm}
-                  value={this.state.bpmInput}
-                  onChange={this.inputBPM}
-                  onKeyUp={this.submitBPM}
-                  onBlur={this.blurInput}/>
+          <button
+            type="button"
+            id="increment"
+            onMouseDown={this.adjustBPM}
+            onMouseUp={this.clearAdjustTimers}
+            onMouseLeave={this.clearAdjustTimers}
+          >
+            {" "}
+            +{" "}
+          </button>
 
-          <button type='button'
-                  id='increment'
-                  onMouseDown={this.adjustBPM}
-                  onMouseUp={this.clearAdjustTimers}
-                  onMouseLeave={this.clearAdjustTimers}> + </button>
-
-          <button type='button'
-                  id='metronome'
-                  onClick={this.toggleMetronome}> {this.state.metronomeAnimation} </button>
+          <button type="button" id="metronome" onClick={this.toggleMetronome}>
+            {" "}
+            {this.state.metronomeAnimation}{" "}
+          </button>
         </div>
 
-
-        <div className='position controller'>
+        <div className="position controller">
           <table>
             <tbody>
               <tr>
-                <td className='data'>{this.state.beats}</td>
+                <td className="data">{this.state.beats}</td>
                 <td>:</td>
-                <td className='data'>{this.state.bars}</td>
+                <td className="data">{this.state.bars}</td>
                 <td>:</td>
-                <td className='data'>{this.state.sixteenths}</td>
+                <td className="data">{this.state.sixteenths}</td>
               </tr>
             </tbody>
           </table>
         </div>
 
-        <div className='playback controller'>
-          <button type='button'
-                  id='play-pause'
-                  className=''
-                  onClick={this.toggleTransport}> ❙▶ </button>
+        <div className="playback controller">
+          <button
+            type="button"
+            id="play-pause"
+            className=""
+            onClick={this.toggleTransport}
+          >
+            {" "}
+            ❙▶{" "}
+          </button>
 
-          <button type='button'
-                  id='stop'
-                  className='active'
-                  onClick={this.stopTransport}> ■ </button>
+          <button
+            type="button"
+            id="stop"
+            className="active"
+            onClick={this.stopTransport}
+          >
+            {" "}
+            ■{" "}
+          </button>
         </div>
-
       </div>
-    )
+    );
   }
 }
 
-export default Transport
+export default Transport;
 
 /* OBJECTIVES
 Options Object?
--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Transport, Tracklist} from './DAW/'
+import React from "react";
+import { Transport, Tracklist } from "./DAW/";
+
+import ToneJsProvider from "./context/ToneJsProvider";
+import ToneJsContext from "./context/ToneJsContext";
 
 const Main = () => {
   return (
-    <div className="Main">
-      <Transport />
-      <Tracklist />
-    </div>
-  )
-}
+    <ToneJsProvider>
+      <ToneJsContext.Consumer>
+        {props => {
+          return (
+            <div className="Main">
+              <Transport toneJsContext={props} />
+              <Tracklist />
+            </div>
+          );
+        }}
+      </ToneJsContext.Consumer>
+    </ToneJsProvider>
+  );
+};
 
 export default Main;
